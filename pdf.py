c6785983c0e646182fde02d19206ec045a6bb302
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2014-12-03 19:11:58 +0000

    Prepare to fix annotation by moving text_to_operations inside SchematicReader and adding a utility function to invert a transformation matrix.

--- pdf.py
+++ pdf.py
@@ -72,48 +72,71 @@ class SchematicReader(PyPDF2.PdfFileReader):
         newContentsArray.append(ContentStream(page.getContents(), page.pdf))
 
         addedContents = NovelContentStream(page.pdf)
-        addedContents.operations = list(text_to_operations(text_items))
+        addedContents.operations = self.text_to_operations(page, text_items)
         newContentsArray.append(addedContents)
 
         newContents = ContentStream(newContentsArray, page.pdf).flateEncode()
 
         page[NameObject('/Contents')] = newContents
 
-def text_to_operations(*args, **kwargs):
-    return objectify_ops(text_to_raw_ops(*args, **kwargs))
+    def text_to_operations(self, *args, **kwargs):
 
-def text_to_raw_ops(text_items, font='/TT2'):
-    yield ([], 'BT')
+        def to_pypdf2_object(x):
+            '''Convert a Python object to a PyPDF2 object.'''
+            if isinstance(x, str):
+                return TextStringObject(x)
+            elif isinstance(x, int):
+                return NumberObject(x)
+            elif isinstance(x, float):
+                return FloatObject('%.2f' % x)
+            else:
+                raise NotImplementedError('unrecognised object: %r' % x)
 
-    # this font size here seems to have no effect, and you
-    # have to change the scale factor in the text matrix
-    yield ([font, 1], 'Tf')
+        def to_operation(operation):
+            return (tuple(map(to_pypdf2_object, operation[0])),
+                    to_pypdf2_object(operation[1]))
 
-    # switch to red text
-    yield (['/DeviceRGB'], 'CS')
-    yield ([255, 0, 0], 'sc')
+        return map(to_operation,
+                self.text_to_unwrapped_operations(*args, **kwargs))
 
-    for text, (x, y), scale in text_items:
-        # 5 was chosen by trial and error to approximately match
-        # size of chars in development doc
-        yield ([0, 5*scale, -5*scale, 0, y, x], 'Tm')
-        yield ([text], 'Tj')
+    def text_to_unwrapped_operations(self, page, text_items):
+        font = '/TT2' # TODO: don't hardcode
 
-    yield ([], 'ET')
+        yield ([], _('BT')
 
-def objectify_ops(ops):
-    def objectify(x):
-        if isinstance(x, str):
-            return TextStringObject(x)
-        elif isinstance(x, int):
-            return NumberObject(x)
-        elif isinstance(x, float):
-            return FloatObject('%.2f' % x)
-        else:
-            raise NotImplementedError('unrecognised object: %r' % x)
+        # this font size here seems to have no effect, and you
+        # have to change the scale factor in the text matrix
+        yield ([font, 1], 'Tf')
 
-    for operands, operation in ops:
-        yield ((objectify(operand) for operand in operands), objectify(operation))
+        # switch to red text
+        yield (['/DeviceRGB'], 'CS')
+        yield ([255, 0, 0], 'sc')
+
+        for text, (x, y), scale in text_items:
+            # 5 was chosen by trial and error to approximately match
+            # size of chars in development doc
+            yield ([0, 5*scale, -5*scale, 0, y, x], 'Tm')
+            yield ([text], 'Tj')
+
+        yield ([], 'ET')
+
+def homogeneous_inverse(hm):
+    '''Calculates the inverse of a 2D homogeneous matrix:
+
+    [[M, T],  -> [[M^-1, M\T],
+     [0, 1]]  ->  [   0,   1]]'''
+
+    m = hm[:-1, :-1]
+    t = hm[:-1, -1]
+
+    result = numpy.array(hm)
+
+    minv = m.invert()
+
+    result[:-1, :-1] = minv
+    result[:-1, -1] = minv.dot(t)
+
+    return result
 
 def line_ops_to_lines(ops):
     lines = []
