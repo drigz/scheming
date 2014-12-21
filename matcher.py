c6c602694ba768f06064cb1665be35eebb36e76b
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2014-12-21 23:31:58 +0000

    Show all ambiguous matches
    This involved switching matches from tuples to basic named tuples to
    keep the start index around for longer. This will help when removing
    ambiguities due to origin, scale & orientation.

--- matcher.py
+++ matcher.py
@@ -5,9 +5,15 @@ import math
 import re
 from intervaltree import Interval, IntervalTree
 
-from collections import Counter
+from collections import defaultdict, Counter
 
 import sigil
+
+class Match(object):
+    def __init__(self, sig, start):
+        self.sig = sig
+        self.start = start
+        self.end = start + len(sig.ops)
 
 def match_sigils(sigdict, abs_ops, skip_alignment_check=False):
     if len(abs_ops) < 2:
@@ -26,14 +32,16 @@ def match_sigils(sigdict, abs_ops, skip_alignment_check=False):
     if not skip_alignment_check:
         matches = check_alignment(sigdict, matches)
 
-    return matches
+        print count_ambiguous(matches)
+
+    return [(m.sig, m.origin, m.sf) for m in matches]
 
 def match_without_scale(sigdict, ops):
     '''Find all possible matches based only on the directions of each line.
 
     Returns an array matches of the form:
-        [(sigil, start, end)]
-    where start and end are the indices of the start & end ops.'''
+        [Match(sigil, start)]
+    where start is the index of the first operation.'''
 
     matches = []
 
@@ -54,7 +62,7 @@ def match_without_scale(sigdict, ops):
             doc_ops = ops[start : start+len(sig.ops)]
 
             if all(match_op(doc_op, sig_op) for (doc_op, sig_op) in zip(doc_ops, sig.ops)):
-                matches.append( (sig, start, start+len(sig.ops)) )
+                matches.append( Match(sig, start) )
 
     return matches
 
@@ -72,55 +80,49 @@ def match_op(step1, step2, tol=0.93):
     return (x1*x2+y1*y2)/n1/n2 > tol
 
 def remove_submatches(matches):
-    '''Given a list of matches [(sigil, start, end)], remove all submatches.
+    '''Given a list of matches, removing all submatches.
 
     A submatch is a match whose operations are all part of a larger match, eg
     matching a hyphen on the crossbar of an A.'''
 
     def end_then_start(match):
-        _, start, end = match
-        return (end, -start)
+        return (match.end, -match.start)
 
     deleted = []
     matches = sorted(matches, key=end_then_start)
 
-    supermatch_sig, supermatch_start, supermatch_end = matches[-1]
+    supermatch = matches[-1]
 
-    for (i, (s, start, end)) in reversed(list(enumerate(matches))):
+    for (i, m) in reversed(list(enumerate(matches))):
 
-        if start > supermatch_start or \
-                (start == supermatch_start and end < supermatch_end):
-            deleted.append((s.char, supermatch_sig.char))
+        if m.start > supermatch.start or \
+                (m.start == supermatch.start and m.end < supermatch.end):
+            deleted.append((m.sig.char, supermatch.sig.char))
             del matches[i]
 
-        if start < supermatch_start:
-            supermatch_sig, supermatch_start, supermatch_end = s, start, end
-
-    # print the frequency of sigils which appear as spurious matches within
-    # other sigils
-    for (k,v) in sorted(Counter(deleted).items()):
-        print k, v
+        if m.start < supermatch.start:
+            supermatch = m
 
     return matches
 
 def check_scales(matches, abs_ops, ops):
-    '''Given a list of matches [(sigil, start, end)], and the ops that they
-    come from, calculate the scale factor of each match.
+    '''Given a list of matches, and the ops that they come from, calculate the
+    scale factor of each match.
 
     Matches with inconsistent scale factors are removed.
 
-    Returns a list [(sigil, origin, scale_factor)].'''
+    Returns a list of matches with the sf and origin members added.'''
 
     processed_matches = []
 
-    for s, start, end in matches:
-        doc_ops = ops[start:end]
+    for m in matches:
+        doc_ops = ops[m.start:m.end]
 
-        doc_sf = sigil.ops_scale(doc_ops) / s.scale
+        doc_sf = sigil.ops_scale(doc_ops) / m.sig.scale
 
         # check scale factor of each operation
         scale_error = False
-        for sig_op, doc_op in zip(s.ops, doc_ops):
+        for sig_op, doc_op in zip(m.sig.ops, doc_ops):
             assert sig_op[1] == doc_op[1], "match_op() isn't doing its job"
 
             sig_n = math.sqrt(sig_op[0][0]**2 + sig_op[0][1]**2)
@@ -135,11 +137,13 @@ def check_scales(matches, abs_ops, ops):
                     scale_error = True
 
         # get absolute position of sig origin
-        start_op = abs_ops[start]
-        origin = [a+b*doc_sf for a,b in zip(start_op[0], s.origin)]
+        start_op = abs_ops[m.start]
+        origin = [a+b*doc_sf for a,b in zip(start_op[0], m.sig.origin)]
 
         if scale_error is False:
-            processed_matches.append((s, origin, doc_sf))
+            m.origin = origin
+            m.sf = doc_sf
+            processed_matches.append(m)
 
     return processed_matches
 
@@ -156,19 +160,25 @@ def check_alignment(sigdict, matches):
     The main advantage to this step is that it disambiguates between hyphens
     and underscores.'''
 
+    # we need V to measure the font
+    if 'V' not in sigdict:
+        return matches
+
     # estimate font metrics based on V, the widest character
     v_width = sigdict['V'][0].width
     gap_width = v_width / 2.58  # gap between adjacent characters
     space_width = gap_width * 2 # the width of a space character
+    epsilon = 0.001 # minimum gap between characters
 
     # for each match, work out the range of possible starts of the next character:
     #   FROM x + width
     #   TO   x + width + gap + space + gap + one more gap for tolerance
     windows = IntervalTree()
-    for i, (s, (x, y), sf) in enumerate(matches):
+    for i, m in enumerate(matches):
+        x, y = m.origin
         windows.addi(
-                x + sf * s.width,
-                x + sf * (s.width + 3 * gap_width + space_width),
+                x + m.sf * m.sig.width + epsilon,
+                x + m.sf * (m.sig.width + 3 * gap_width + space_width),
                 (i, y))
 
     # work out which matches are legit (because they are > 1 op) and which are
@@ -177,11 +187,12 @@ def check_alignment(sigdict, matches):
     aligned_with = dict()
 
     max_y_sep = 0.7
-    for i, (s, (x, y), sf) in enumerate(matches):
+    for i, m in enumerate(matches):
+        x, y = m.origin
 
         # only delete single-operation matches as we're mainly worried about
         # hyphens and underscores
-        if len(s.ops) > 1:
+        if len(m.sig.ops) > 1:
             legit_indices.add(i)
             continue
 
@@ -215,7 +226,7 @@ def check_alignment(sigdict, matches):
 
         return result
 
-    print 'deleting', Counter(s.char for i, (s, pos, sf) in
+    print '[check_alignment]: deleting', Counter(m.sig.char for i, m in
             enumerate(matches) if not check_is_legit(i))
 
     # finally, exclude invalid matches
@@ -224,3 +235,18 @@ def check_alignment(sigdict, matches):
 
     return matches
 
+def count_ambiguous(matches):
+    '''Return a Counter() of sigils matching the same operations in the
+    document.'''
+
+    sigils_by_position = defaultdict(list)
+    for m in matches:
+        sigils_by_position[(m.start, len(m.sig.ops))].append(m)
+
+    ctr = Counter()
+    for ambiguous in sigils_by_position.values():
+        if len(ambiguous) > 1:
+            chars = tuple(sorted(m.sig.char for m in ambiguous))
+            ctr[chars] += 1
+
+    return ctr
